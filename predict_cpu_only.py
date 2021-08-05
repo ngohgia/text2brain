e096914db334990538b2cee9fbd8b546896c87a2
Minh Nguyen <minhnb3192@gmail.com>
2021-08-05 17:49:21 -0400

    correct path to checkpoint

--- predict_cpu_only.py
+++ predict_cpu_only.py
@@ -1,38 +1,32 @@
-import os
 import sys
 import numpy as np
 
 import torch
-from torch.nn.parameter import Parameter
 import nibabel as nib
 
 from models.text2brain_model import Text2BrainModel
-
-PHRASES = {
-            "false-belief_tale_vs_mecha_tale": "False-belief tale vs mecha tale",
-            "false-belief_tale_vs_mechanistic_tale": "False-belief tale vs mechanistic tale",
-          }
 
 
 if __name__ == "__main__":
 
     query = sys.argv[1]
     output_file = sys.argv[2]
+    fc_channels = 64
+    decoder_filters = 32
 
-    checkpoint_file = "checkpoints/fc64_d128_relu_lr0.03_decay1e-06_drop0.55_seed28_checkpoint.pth"
+    checkpoint_file = f"checkpoints/fc{fc_channels}_d{decoder_filters}_relu_lr0.03_decay1e-06_drop0.55_seed28_checkpoint.pth"
     pretrained_bert_dir = "scibert_scivocab_uncased"
 
     """Init Model"""
     model = Text2BrainModel(
         out_channels=1,
-        fc_channels=64,
-        decoder_filters=32,
+        fc_channels=fc_channels,
+        decoder_filters=decoder_filters,
         pretrained_bert_dir=pretrained_bert_dir,
         drop_p=0.55)
 
     state_dict = torch.load(checkpoint_file, map_location=torch.device('cpu'))['state_dict']
     model.load_state_dict(state_dict)
-    
     model.eval()
 
     """ Output brain image """
@@ -42,12 +36,11 @@ if __name__ == "__main__":
        [   0.,    0.,    4.,  -72.],
        [   0.,    0.,    0.,    1.]])
 
+    text = (query.replace("/", ""), )
     with torch.no_grad():
-        text = (query.replace("/", ""), )
+        pred = model(text).numpy().squeeze(axis=(0, 1))
 
-        pred = model(text).numpy().squeeze(0).squeeze(0)
+    vol_data[3:-3, 3:-4, :-6] = pred
 
-        vol_data[3:-3, 3:-4, :-6] = pred
-        
-        pred_img = nib.Nifti1Image(vol_data, affine)
-        nib.save(pred_img, output_file)
+    pred_img = nib.Nifti1Image(vol_data, affine)
+    nib.save(pred_img, output_file)
