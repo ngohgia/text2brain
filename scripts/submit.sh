920a096feecea6aa39e2748a3840f1e2fe734cfa
ngo.h.gia <ngohoanggia@gmail.com>
2021-10-25 14:54:16 -0400

    Fix submit script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch -p sablab-highprio --mem=28G --gres=gpu:1 --job-name=$1 -e ./job_err/%j-$1.err -o ./job_out/%j-$1.out $2
+sbatch -p sablab --mem=28G --gres=gpu:1 --job-name=$1 -e ./job_err/%j-$1.err -o ./job_out/%j-$1.out $2
